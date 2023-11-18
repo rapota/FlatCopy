b1d0c8813b9eb188f30db7a2193d43a66ec01a05
Oleksandr Rapota <rapota84@gmail.com>
2023-11-18 14:41:19 -0800

    Migrated to .net 8

--- publish.cmd
+++ publish.cmd
@@ -1,3 +1,4 @@
-dotnet publish FlatCopy --self-contained --configuration Release --output FlatCopyApp
+dotnet publish FlatCopy --self-contained --configuration Release --output FlatCopy-app
+dotnet publish FlatCopy --self-contained --configuration Release --output FlatCopy-single /p:PublishSingleFile=true
 
 PAUSE
