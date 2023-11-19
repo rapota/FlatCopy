a041a7ab2c8ee5a22a4db9fa3126d600ddd3a90d
Oleksandr Rapota <rapota84@gmail.com>
2023-11-19 11:17:18 -0800

    Added AOT.

--- publish.cmd
+++ publish.cmd
@@ -1,5 +1,7 @@
-dotnet publish FlatCopy --self-contained --configuration Release --output FlatCopy-app
-dotnet publish FlatCopy --self-contained --configuration Release --output FlatCopy-single /p:PublishSingleFile=true
-dotnet publish FlatCopy --self-contained --configuration Release --output FlatCopy-trimmed /p:PublishTrimmed=true
+dotnet publish FlatCopy --configuration Release --output FlatCopy-app --self-contained
+dotnet publish FlatCopy --configuration Release --output FlatCopy-single --self-contained /p:PublishSingleFile=true
+dotnet publish FlatCopy --configuration Release --output FlatCopy-trimmed --self-contained /p:PublishTrimmed=true
+dotnet publish FlatCopy --configuration Release --output FlatCopy-single-trimmed --self-contained /p:PublishSingleFile=true /p:PublishTrimmed=true
+dotnet publish FlatCopy --configuration Release --output FlatCopy-aot-x64 --runtime win-x64 /p:PublishAot=true
 
 PAUSE
